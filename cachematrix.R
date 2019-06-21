e205a54947d8fbb5ea3ef50ec42c1a057be609f5
Suyoung Jang <suyoung3360@gmail.com>
2019-06-22 00:49:18 +0900

    revision length of description

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-#The following two functions take the matrix as an argument and calculate the inverse matrix. The result of the calculation can be stored in the cache memory, allowing efficient calculation to recall the saved value.
+## The following two functions take the matrix as an argument and calculate the inverse matrix.
+## The result of the calculation can be stored in the cache memory, allowing efficient calculation to recall the saved value.
 
 ## Write a short comment describing this function
-# The makeCacheMatrix function takes a matrix as an argument and stores it in x. If it is a matrix that already computes an inverse matrix, this function has its cache data.
+## The makeCacheMatrix function takes a matrix as an argument and stores it in x. 
+## If it is a matrix that already computes an inverse matrix, this function has its cache data.
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
         set <- function(y) {
@@ -21,7 +23,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-# The cacheSolve function receives the matrix argument as the return value of makeCacheMatrix and computes the inverse matrix. If the cache data already contains a calculated value, a message is displayed.
+## The cacheSolve function receives the matrix argument as the return value of makeCacheMatrix
+## and computes the inverse matrix. If the cache data already contains a calculated value, 
+## a message is displayed.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         i<- x$getsolve()
